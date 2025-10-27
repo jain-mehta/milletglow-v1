13fb0ffc731f2ad331e6760d7c824ff5e948742b
jain-mehta <arcuss.development@gmail.com>
2025-10-27 18:48:04 +0530

    commit

--- app/adminpanel/[[...tool]]/page.tsx
+++ app/adminpanel/[[...tool]]/page.tsx
@@ -3,8 +3,6 @@ import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
 
 export const dynamic = "force-dynamic";
-export const runtime = "nodejs";
-export const revalidate = 0;
 
 
 export default function StudioPage() {
